0be0a67a5679ac06a15dece892acd8e2464740b4
johannpa <johann_paque@hotmail.fr>
2024-05-13 18:49:29 +0200

    Import redux with Provider and store from our index.js in store then use them in the src index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import "./global.css";
 import { App } from "./App";
-import React, { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+);
