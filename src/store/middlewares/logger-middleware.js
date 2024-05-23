04bb3702d2eb5f9f17fe9244c969313af49a51be
johannpa <johann_paque@hotmail.fr>
2024-05-23 16:59:03 +0200

    Now we can do the same thing like earlier

--- src/store/middlewares/logger-middleware.js
+++ src/store/middlewares/logger-middleware.js
@@ -1,11 +1,13 @@
-import { createListenerMiddleware } from "@reduxjs/toolkit";
+import { createListenerMiddleware, isAnyOf } from "@reduxjs/toolkit";
+import { addExpense, setIncome } from "store/expense/expense-slice";
 
 export const loggerMiddleware =  createListenerMiddleware();
 
 loggerMiddleware.startListening({
-    predicate:(action) => {
-        return action.type === "expenseSlice/addExpense" || action.type === "expenseSlice/setIncome";
-    },
+    // predicate:(action) => {
+    //     return action.type === "expenseSlice/addExpense" || action.type === "expenseSlice/setIncome";
+    // },
+    matcher: isAnyOf(addExpense, setIncome),
     effect: async (action, listenerAPI) => {
         console.log(action);
         console.log(listenerAPI.getState());
