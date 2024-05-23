aed2bd166f0d9aeb38a5d75d28046c06bc1dd7db
johannpa <johann_paque@hotmail.fr>
2024-05-23 10:42:39 +0200

    Imports for the new configuration

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,7 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import { expenseSlice } from "./expense/expense-slice";
+import storage from "redux-persist/lib/storage";
+import { persistStore, persistReducer } from "redux-persist";
 
 const store = configureStore({
     reducer: {
