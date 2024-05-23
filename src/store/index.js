be4d7cf5066956c71b3c80f58288b082a4884005
johannpa <johann_paque@hotmail.fr>
2024-05-23 10:49:45 +0200

    Continue the configuration

--- src/store/index.js
+++ src/store/index.js
@@ -3,10 +3,21 @@ import { expenseSlice } from "./expense/expense-slice";
 import storage from "redux-persist/lib/storage";
 import { persistStore, persistReducer } from "redux-persist";
 
+
+const persistConfig = {
+    key: "root",
+    version: 1,
+    storage,
+}
+
+const rootReducers = combineReducers({
+    EXPENSE: expenseSlice.reducer,
+});
+
+const persistedReducers = persistReducer(persistConfig, rootReducers);
+
 const store = configureStore({
-    reducer: {
-        EXPENSE: expenseSlice.reducer,
-    },
+    reducer: persistedReducers,
 });
 
 export { store };
